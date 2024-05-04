a4fd3f1c5dff5173edb8ba5592b776522d3ee669
JSeokjun <sjjjang00@gmail.com>
2024-05-04 18:18:30 +0900

    이미지변환 백 연동, 로딩창, 다운로드 기능 추가

--- web/src/Pages/Generator/Generator.js
+++ web/src/Pages/Generator/Generator.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import './Generator.css';
-import { Link } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom'
 
 export default function Generator() {
   const [image, setImage] = useState(null);
   const [concept, setConcept] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleImageChange = (event) => {
     const selectedImage = event.target.files[0];
     setImage(selectedImage);
   };
-
 
   const handleConceptChange = (event) => {
     const { value } = event.target;
@@ -23,14 +22,36 @@ export default function Generator() {
     }
   };
 
-  const convertImage = () => {
-    console.log("이미지 변환 함수 호출");
-    console.log("선택된 이미지:", image);
-    console.log("선택된 컨셉:", concept);
+  const handleSubmit = async () => {
+    setIsLoading(true);
+
+    const formData = new FormData();
+    formData.append('file', image);
+    formData.append('prompt', concept.toString());
+  
+    try {
+      const response = await fetch('http://localhost:50/ImageConversion', {
+        method: 'POST',
+        body: formData,
+      });
+      const result = await response.json();
+
+      navigate('/Pages/Generator/Output', { state: { imageUrl: result.image_url } });
+      console.log(result)
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <div className="containers">
+      {isLoading && (
+        <div className="loading-modal">
+          <div className="loading-text">이미지를 변환중이에요! 잠시만 기다려주세요.</div>
+        </div>
+      )}
       <div className="image-1">이미지 변환하기 :2 사진을 다양한 그림으로 바꿔봐요!</div>
       <div className="image">Step1. 사진 선택하기</div>
       <div className="image-2">그림으로 바꾸고 싶은 사진을 컴퓨터에서 골라주세요.</div>
@@ -42,30 +63,33 @@ export default function Generator() {
       <div className="dividing" />
       <div className='concept-2'>
         <label className="radio-label">
-          <input type="radio" name="concept" value="디즈니" onChange={handleConceptChange} /> 
-          <span className="radio-custom"></span>
-          <span className="radio-text">디즈니</span>
-        </label>
-        <label className="radio-label">
           <input type="radio" name="concept" value="젊은시절" onChange={handleConceptChange} /> 
           <span className="radio-custom"></span>
           <span className="radio-text">젊은시절</span>
         </label>
         <label className="radio-label">
-          <input type="radio" name="concept" value="손그림" onChange={handleConceptChange} /> 
+          <input type="radio" name="concept" value="디즈니" onChange={handleConceptChange} /> 
           <span className="radio-custom"></span>
-          <span className="radio-text">손그림</span>
+          <span className="radio-text">디즈니</span>
+        </label>
+        <label className="radio-label">
+          <input type="radio" name="concept" value="픽사" onChange={handleConceptChange} /> 
+          <span className="radio-custom"></span>
+          <span className="radio-text">픽사</span>
+        </label>
+        <label className="radio-label">
+          <input type="radio" name="concept" value="사이보그" onChange={handleConceptChange} /> 
+          <span className="radio-custom"></span>
+          <span className="radio-text">사이보그</span>
         </label>
       </div>
 
       <div className="image-3">다 선택하셨나요? 이제 한 번 바꿔봐요! 아래의 버튼을 클릭해주세요.</div>
       <div className="last">
-        <Link to="/Pages/Generator/Output" className="go">
-          <div className="button">
+          <div className="button" onClick={handleSubmit}>
             바꾸기!
           </div>
-        </Link>
-      </div>
+      </div>ㄴ
     </div>
   );
 }
--- web/src/Pages/Generator/Output.js
+++ web/src/Pages/Generator/Output.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import './Output.css';
 import Gimage from './Gimage.jpeg';
-
-
-
+import { useLocation } from 'react-router-dom';
 
 export default function Output() {
+
+  const location = useLocation();
+  const { imageUrl } = location.state || { imageUrl: '' };
+
+  const downloadImage = async () => {
+    try {
+      const imageSrc = imageUrl || Gimage;
+      const image = await fetch(imageSrc);
+      const imageBlob = await image.blob();
+      const imageURL = URL.createObjectURL(imageBlob);
+
+      const element = document.createElement('a');
+      element.href = imageURL;
+      element.download = 'resultImage.jpg';
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+
+      URL.revokeObjectURL(imageURL);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
     return (
-      <div class='outmain'>
-        <div class='output'>
+      <div className='outmain'>
+        <div className='output'>
           <div> ~ 짜잔 이미지가 생성되었어요 ! ~ </div>
-          <div class='outimage'>
-            <img src={Gimage} alt="Logo" />
+          <div className='outimage'>
+            {imageUrl ? <img src={imageUrl} alt="Converted" /> 
+                      : <img src={Gimage} alt="Default"/>}
           </div>
-
         </div>
-
-        <div class='outright'>
-          <div class='outbutton'>
-            <button>📥  다운로드 하기 </button>
+        <div className='outright'>
+          <div className='outbutton'>
+            <button onClick={downloadImage}>📥  다운로드 하기 </button>
           </div>
-
-
         </div>
       </div>
-
     );
   }
